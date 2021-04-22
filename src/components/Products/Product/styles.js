a756080b880962de056aee7310f2a655ee17365c
jwaugh3 <jwaugh3@gmail.com>
2021-04-22 14:33:56 -0500

    removed nonessential dependencies

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   root: {
-    maxWidth: '100%',
+    maxWidth: '800px',
+    width: '100%',
     height: '100%',
     [theme.breakpoints.down('sm')]: {
       width: '80%',
